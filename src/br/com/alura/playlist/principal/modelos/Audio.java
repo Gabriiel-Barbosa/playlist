919501e6c34b953cd826b12bdbfd84d583f82e25
Gabriel <gabrielbarbosa.j01@gmail.com>
2023-06-19 08:53:04 -0300

    Estrutura do Projeto

--- src/br/com/alura/playlist/principal/modelos/Audio.java
+++ src/br/com/alura/playlist/principal/modelos/Audio.java
@@ -1,2 +1,37 @@
-package br.com.alura.playlist.principal.modelos;public class Audio {
+package br.com.alura.playlist.principal.modelos;
+
+public class Audio {
+    private String Titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private double classificacao;
+
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public void curte(){
+        this.totalCurtidas ++;
+    }
+
+    public void reproduz(){
+        this.totalReproducoes ++;
+    }
 }
--- src/br/com/alura/playlist/principal/modelos/Podcast.java
+++ src/br/com/alura/playlist/principal/modelos/Podcast.java
@@ -1,2 +1,23 @@
-package br.com.alura.playlist.principal.modelos;public class Podcast {
+package br.com.alura.playlist.principal.modelos;
+
+public class Podcast extends Audio {
+    private String apresentador;
+    private String descricao;
+
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
